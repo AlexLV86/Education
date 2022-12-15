026aeb3596cd68f27898e391eb3be37b4d72a164
AlexLV86 <canlik@mail.ru>
2022-12-15 21:12:09 +1000

    First commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"module7/calc"
+)
+
+func main() {
+	var n1, n2 float64
+	var op string
+	_, err := fmt.Scanln(&n1)
+	if err != nil {
+		return
+	}
+	_, err = fmt.Scanln(&op)
+	if err != nil {
+		return
+	}
+	_, err = fmt.Scanln(&n2)
+	if err != nil {
+		return
+	}
+	calcul := calc.NewCalculator()
+	fmt.Println("Результат вычислений: ")
+	fmt.Println(calcul.Calculate(n1, n2, op))
+}
