220a2de11c1f412e6846387bc103dea6556e87a5
AlexLV86 <canlik@mail.ru>
2022-12-16 17:43:58 +1000

    lll

--- main.go
+++ main.go
@@ -23,4 +23,5 @@ func main() {
 	calcul := calc.NewCalculator()
 	fmt.Println(calcul.Calculate(n1, n2, op))
 	fmt.Println(calcul.Calculate(n1, n2, op))
+	fmt.Println(calcul.Calculate(n1, n2, op))
 }
